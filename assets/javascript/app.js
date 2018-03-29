19cc67230d7313f81708e684fa3c1ff1f9576847
Michael Myers <myersmyer@gmail.com>
2018-03-28 23:59:12 -0400

    added basic countdown

---
+++ assets/javascript/app.js
@@ -0,0 +1,40 @@
+var startTimeout;
+var countDown = 40;
+var intervalId;
+
+$(document).ready(function () {
+
+// setting the run function 
+    function run() {
+        clearInterval(intervalId);
+        intervalId = setInterval(decrement, 1000);
+    }
+
+// setting the countdown function 
+    function decrement() {
+        countDown--;
+        $("#counter").html("<h2>" + countDown + "</h2>");
+        if (countDown === 0) {
+            stop();
+            console.log("My Stop has worked");
+            alert("You fuggin lost!");
+
+        }
+    }
+
+// setting the stop function
+    function stop() {
+        clearInterval(intervalId);
+    }
+
+//  will likely pull this out. Just testing a countdown
+    $("#start").on("click", function () {
+        console.log("click worked");
+        startTimeout = setTimeout(function () {
+            alert("Out of time!");
+        }, 1000 * 10);
+    });
+
+    run();    
+
+});
