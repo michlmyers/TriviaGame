69d26a8a386cbcd79d7a8b050239219abbe1a314
Michael Myers <myersmyer@gmail.com>
2018-03-31 10:52:36 -0400

    added some button logs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,41 @@
 var startTimeout;
-var countDown = 40;
+var countDown = 10;
 var intervalId;
+var myAnswers = ['Tesla', 'Gremlins', 'Ghostbusters', 'Stanley Kubrick', 'Tony Montana'];
 
 $(document).ready(function () {
 
     $("#start").on("click", run);
-// setting the run function 
+
+    // setting the run function 
     function run() {
+        // unhide the game questions
+        $('#fullGame').removeClass('hidden');
         clearInterval(intervalId);
         intervalId = setInterval(decrement, 1000);
+
+        // set correct answers
+
+        // capture user selection and place to variable
+        $('#questionOne input').on('click', function() {
+            console.log($('input[name=backFuture]:checked', '#questionOne').val()); 
+        });
+        $('#questionTwo input').on('click', function() {
+            console.log($('input[name=gizMo]:checked', '#questionTwo').val()); 
+        });
+        $('#questionThree input').on('click', function() {
+            console.log($('input[name=gBuster]:checked', '#questionThree').val()); 
+        });
+        $('#questionFour input').on('click', function() {
+            console.log($('input[name=shine]:checked', '#questionFour').val()); 
+        });
+        $('#questionFive input').on('click', function() {
+            console.log($('input[name=alPac]:checked', '#questionFive').val()); 
+        });
+
     }
 
-// setting the countdown function 
+    // setting the countdown function 
     function decrement() {
         countDown--;
         $("#counter").html("<h2>" + countDown + "</h2>");
@@ -22,19 +46,9 @@ $(document).ready(function () {
         }
     }
 
-// setting the stop function
+    // setting the stop function
     function stop() {
         clearInterval(intervalId);
     }
-
-/*  will likely pull this out. Just testing a countdown
-    $("#start").on("click", function () {
-        console.log("click worked");
-        startTimeout = setTimeout(function () {
-            alert("Out of time!");
-        }, 1000 * 10);
-    }); */
-
-    // run();    
 
 });
